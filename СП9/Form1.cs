dcccc68d10b06ba1985127d91924b85abf347e88
Coffey <H1.coffey@bk.ru>
2024-03-23 02:59:53 +0700

    9

---
+++ СП9/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+
+namespace СП9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var screen in Screen.AllScreens)
+            {
+                listBox1.Items.Add("Имя устройства: " + screen.DeviceName);
+                listBox1.Items.Add("Границы: " + screen.Bounds.ToString());
+                listBox1.Items.Add("Тип: " + screen.GetType().ToString());
+                listBox1.Items.Add("Рабочая область: " + screen.WorkingArea.ToString());
+                listBox1.Items.Add("Главный экран: " + screen.Primary.ToString());
+            }
+        }
+    }
+}
